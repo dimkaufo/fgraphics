b16f6be1294025400d170a08b7f8a5db85987e4d
Mark Dolbyrev <mark.dolbyrev@lcsas-MacBook-Pro-2.local>
2016-09-09 09:35:37 +0300

    Improvement of finding objects under a point.

--- dist/index.js
+++ dist/index.js
@@ -9,6 +9,9 @@ __export(require('./adapter/pixi/PixiAdapter'));
 __export(require('./tools/display/DisplayObjectTools'));
 __export(require('./tools/display/DisplayObjectWithNameVO'));
 __export(require('./tools/display/MovieClipTools'));
+__export(require('./adapter/abstract/wrapper/display/AnimatableSpriteWrapperEvent'));
+__export(require('./adapter/abstract/wrapper/display/DisplayObjectWrapperMouseEvent'));
+__export(require('./adapter/abstract/wrapper/display/TextWrapperAlign'));
 __export(require('./adapter/abstract/wrapper/ticker/TickerEvent'));
 __export(require('./adapter/pixi/wrapper/display/PixiDisplayObjectContainerWrapper'));
 __export(require('./adapter/pixi/wrapper/display/PixiDisplayObjectWrapper'));
@@ -17,7 +20,4 @@ __export(require('./adapter/pixi/wrapper/display/PixiMouseEvent'));
 __export(require('./adapter/pixi/wrapper/display/PixiSpriteWrapper'));
 __export(require('./adapter/pixi/wrapper/display/PixiTextWrapper'));
 __export(require('./adapter/pixi/wrapper/ticker/PixiTickerWrapper'));
-__export(require('./adapter/abstract/wrapper/display/AnimatableSpriteWrapperEvent'));
-__export(require('./adapter/abstract/wrapper/display/DisplayObjectWrapperMouseEvent'));
-__export(require('./adapter/abstract/wrapper/display/TextWrapperAlign'));
 //# sourceMappingURL=index.js.map
